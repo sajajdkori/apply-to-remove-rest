32dcb64a6a5a3fb6a7edad60e70a5fa50d8e29f1
sajajdkori <roughwork561@gmail.com>
2026-02-27 03:40:25 +0500

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-export default {
-  async fetch(request) {
-    // The URL you want people to be sent to
-    const destinationURL = "https://apply-to-remove-restrictionn.netlify.app";
-
-    // This creates a 301 (Permanent) redirect
-    return Response.redirect(destinationURL, 301);
-  },
-};
